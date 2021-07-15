4fe4771b7a53ea094a3da159b21c39a7cea09587
Amador Copado <Daniel.Copado@ibm.com>
2021-07-14 21:34:09 -0500

    Create Matriz
    Show Matriz on console

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -2,4 +2,17 @@ function ticTacToe() {
     return true;
 }
 
+
+function printMatriz(matriz){
+    var matrizPrint = ""
+    for(var index = 0; index < (matriz.length)/3; index++){
+        for(var jindex = 0; jindex < (matriz.length)/3; jindex++){
+            matrizPrint += jindex == 2 ? ' ' : ' |';
+        }
+        matrizPrint += index == 2 ? '' : '\n'+'-+-+-'+'\n';
+    }
+    console.log(matriz);
+    console.log(matrizPrint);
+}
+
 module.exports = ticTacToe;
